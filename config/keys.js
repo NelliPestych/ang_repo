dbd4d7519aaf62ace06b28bad7647a9978d62db8
NelliPestych <NelliPestych@bigmir.net>
2022-04-13 13:58:01 +0300

    initial

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://nelli_admin:ifhbr171123@cluster0.qnmup.mongodb.net/<dbname>?retryWrites=true&w=majority',
-  jwt: 'dev-jwt'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.dev')
 }
